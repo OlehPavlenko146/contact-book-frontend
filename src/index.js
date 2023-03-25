db9239abe089b0f922ffe77b1a47e65ae046ee14
Oleh Pavlenko <a310thunderbolt@gmail.com>
2023-03-25 22:45:56 +0200

    deleted basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { App } from 'components/App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
